898231c701205922003dab76292bee2cb30ecc96
AlwinBrauns <63373755+AlwinBrauns@users.noreply.github.com>
2020-06-20 22:48:25 +0200

    started drawing

--- expWithCstmPnter/lib/clock_view.dart
+++ expWithCstmPnter/lib/clock_view.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class ClockView extends StatefulWidget {
@@ -20,13 +22,59 @@ class _ClockViewState extends State<ClockView> {
 
 class ClockPainter extends CustomPainter {
   @override
-  void paint(Canvas canva, Size size) {
+  void paint(Canvas canvas, Size size) {
+    var centerX = size.width / 2;
+    var centerY = size.height / 2;
+    var center = Offset(centerX, centerY);
+    var radius = min(centerX, centerY);
 
+    // ( Paint()..color = ) <=> ( var brush = Paint(); brush.color = )
+    var fillBrush = Paint()..color = Colors.blueGrey[900];
+    var outlineBrush = Paint()
+      ..color = Colors.blueGrey[100]
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 15;
+
+    var secBrush = Paint()
+      ..shader = RadialGradient(colors: [
+        Colors.lightBlue,
+        Colors.pink,
+      ]).createShader(Rect.fromCircle(center: center,radius: radius,))
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 9
+      ..strokeCap = StrokeCap.round;
+
+      var minBrush = Paint()
+      ..shader = RadialGradient(colors: [
+        Colors.lightGreen,
+        Colors.blue,
+      ]).createShader(Rect.fromCircle(center: center,radius: radius,))
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 12
+      ..strokeCap = StrokeCap.round;
+
+      var hourBrush = Paint()
+      ..shader = RadialGradient(colors: [
+        Colors.red,
+        Colors.green,
+      ]).createShader(Rect.fromCircle(center: center,radius: radius,))
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 14
+      ..strokeCap = StrokeCap.round;
+      
+
+    canvas.drawCircle(center, radius - 40, fillBrush);
+    canvas.drawCircle(center, radius - 40, outlineBrush);
+    canvas.drawLine(center, Offset(150, 100), hourBrush);
+    canvas.drawLine(center, Offset(175, 80), minBrush);
+    canvas.drawLine(center, Offset(100, 75), secBrush);
+    canvas.drawCircle(center, 0 + (radius / 12), fillBrush
+    ..color = Colors.blueGrey[100]);
+    
   }
 
   @override
   bool shouldRepaint(CustomPainter oldDelegate) {
-
     return true;
   }
 }
