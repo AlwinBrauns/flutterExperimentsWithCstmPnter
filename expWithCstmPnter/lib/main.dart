82d30dc53124f13c6586faad48fdbd7a2a4db491
AlwinBrauns <63373755+AlwinBrauns@users.noreply.github.com>
2020-06-20 22:01:28 +0200

    Created the flutter project

---
+++ expWithCstmPnter/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Cstm Pnter Stuff',
+      home: HomeScreen(),
+      
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(),
+      
+    );
+  }
+}
